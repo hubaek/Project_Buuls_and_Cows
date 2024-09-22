d403d8d304bfc8bf87cb9999e6e641956235950b
hubaek <zjfl3122@naver.com>
2024-09-23 03:06:44 +0900

    Feat : countStrike 메서드 추가, 정답일치확인 로직추가 Refactor : 정답 변수명 변경 Feat - countStrike : 각 자리와 수가 일치하는 count세고 결과 반환 - break문을 두기위해 정답일치 로직 미리 만들어두기
    Refactor
    - 정답 값을 담은 변수 threeDigitNumber를 직관적인 answer로 변수명 변경

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class BaseballGame {
 
+    // 인스턴스 변수 선언(필드) - 다른 메서드에서 정답을 비교하기 위함
+    private final int answer;
+
     // 객체 생성시 정답을 만들기
     public BaseballGame() {
         // 1~9의 숫자를 리스트에 넣음
@@ -18,8 +21,8 @@ public class BaseballGame {
         int digit3 = numbers.get(2);
 
         // 3자리 수 만드는 법? 1,2,3 만들고 이어 붙일 수있을까?
-        int threeDigitNumber = digit1*100 + digit2 * 10 + digit3;
-        System.out.println(threeDigitNumber);
+        this.answer = digit1*100 + digit2 * 10 + digit3;
+        System.out.println(answer);
 
         System.out.println("<게임을 시작합니다>");
 
@@ -28,6 +31,7 @@ public class BaseballGame {
     public int play() {
         while (true) {
             // 입력값 받기
+            System.out.println("확인용");
             Scanner sc = new Scanner(System.in);
             String inputAnswer = sc.next();
 
@@ -35,7 +39,14 @@ public class BaseballGame {
             if (validateInput(inputAnswer)) {
                 System.out.println("3자리수 검증 완");
 
-                break;
+
+
+                // 정답 일치하는지 확인
+                int parseAnswer = Integer.parseInt(inputAnswer);
+                if (parseAnswer == answer) {
+                    System.out.println("정답일치 확인");
+                    break;
+                }
             } else {
                 System.out.println("다시 정답 입력");
             }
@@ -66,6 +77,30 @@ public class BaseballGame {
         System.out.println("3자리 아님");
         return false;
     }
+
+
+    private int countStrike(String inputAnswer) {
+        // answer(int)와 inputAnswer(String) 같은 숫자와 자리수 일치하면 strike
+
+        // 비교하기 위해 int타입의 정답을 String타입으로 변환
+        String answerStr = String.valueOf(answer);
+
+        int strikeCount = 0;
+
+        for (int i=0; i < answerStr.length(); i++) {
+            // 각 자리의 값이 같으면 count 증가
+            if (answerStr.charAt(i) == inputAnswer.charAt(i)) {
+                strikeCount++;  // 같은 자리에 같은 숫자가 있으면 카운트 증가;
+            }
+        }
+
+        return strikeCount;
+    }
+
+    private int countBall(String inputAnswer) {
+        return 0;
+    }
+
 
 
 }
