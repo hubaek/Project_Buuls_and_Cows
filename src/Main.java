a627bd17e7610de7cc9248402364df1e41d162a3
hubaek <zjfl3122@naver.com>
2024-09-23 17:53:28 +0900

    Feat : 게임 메뉴 선택 로직 추가 - 1. 게임시작하기 : 새로운 정답생성 후 새게임 시작 - 2. 게임기록보기(미완성) : 게임기록을 보여주는 메뉴 - 3. 종료하기 : 게임을 끝내는 메뉴

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,36 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        // 베이스볼 객체 생성시, 게임시작 - 정답생성
-        BaseballGame baseballGame = new BaseballGame();
+        Scanner sc = new Scanner(System.in);
+        // 환영문구
+        System.out.println("숫자야구 게임에 오신걸 환영합니다. ");
+
+        while (true) {
+            System.out.println("원하시는 번호를 입력해 주세요");
+            System.out.println("1. 게임시작하기 2. 게임기록보기 3. 종료하기");
+
+            int menuSelection = sc.nextInt();
+            // 1. 게임시작하기 2. 게임기록보기 3. 종료하기
+            if (menuSelection == 1) {
+                System.out.println("<게임을 시작합니다>");
+                // 베이스볼 객체 생성시, 게임시작 - 정답생성
+                BaseballGame baseballGame = new BaseballGame();
+                // 게임시작 (정답 입력)
+                baseballGame.play();
+            } else if (menuSelection == 2) {
+                // 게임 기록 보기 로직
+                System.out.println("게임기록 보기");
+            } else if (menuSelection == 3) {
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
+
+        }
+
+
+
+
+
 
     }
 }
